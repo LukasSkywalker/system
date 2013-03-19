ec55652fe72cbeee87a291e8af5fc0502e7fe7db
Lukas Diener <lukas.diener@hotmail.com>
2013-03-19 15:56:21 +0100

    New backup script for DB, keep 30 dumps

--- backup_db.sh
+++ backup_db.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-suffix=$(date +%w)
+suffix=$(date +%Y-%m-%d_%H.%M.%S)
 wdir=/home/teampse4/backup
+# count existing backups
+count=ls ${wdir} -1 | wc -l
 
-rm $wdir/mongo-$suffix.tar.gz -rf
+# remove oldest if more than 29
+if [ $count -gt 29 ]
+then
+  rm `ls ${wdir} | sort -n | head -1`
+fi
+
+# create new backup and compress it
 mongodump -o $wdir/mongo-$suffix
 tar -czf $wdir/mongo-$suffix.tar.gz $wdir/mongo-$suffix
 rm -rf $wdir/mongo-$suffix
