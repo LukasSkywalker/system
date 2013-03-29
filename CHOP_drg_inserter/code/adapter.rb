610249aed306d8435aedee9e213a6445a6717433
Gian-Luca Mateo <gianluca.mateo@gmail.com>
2013-03-29 21:58:34 +0100

    Connector for CHOP-DRG added. Slow as hell and atm only for german, also inserting into wront collection.
    But: working
    
    	modified:   CHOP_drg_inserter/.idea/CHOP_drg_inserter.iml
    	modified:   CHOP_drg_inserter/.idea/workspace.xml
    	modified:   CHOP_drg_inserter/Gemfile
    	modified:   CHOP_drg_inserter/Gemfile.lock
    	modified:   CHOP_drg_inserter/code/adapter.rb
    	modified:   CHOP_drg_inserter/code/parser.rb
    	modified:   CHOP_drg_inserter/code/runscript.rb
    	modified:   who_icd_parser/.idea/workspace.xml

--- CHOP_drg_inserter/code/adapter.rb
+++ CHOP_drg_inserter/code/adapter.rb
@@ -2,17 +2,33 @@ require 'mongo'
 include Mongo
 
 class Adapter
-  attr_accessor :mongo_client, :db, :coll
-  def initialize (db , collection ,host, port)
+  attr_accessor :mongo_client, :db, :coll, :write
+  def initialize (db , collection ,host, port, write)
     mongo_client = MongoClient.new(host, port)
-    puts 'Enter your PW for the account pse4_write: '
-    mongo_client.db('admin').authenticate('pse4_write',gets.chomp)
+    self.write = write
+    if write
+      puts 'Enter your PW for the account pse4_write: '
+      mongo_client.db('admin').authenticate('pse4_write',gets.chomp)
+    else
+      mongo_client.db('admin').authenticate('pse4_read','plokij')
+    end
     db = mongo_client.db(db)
     self.coll = db.collection(collection)
   end
 
-  def get_entries ()
-    self.coll.find().to_a
+  def get_entries
+    results = []
+    puts self.coll.find().count
+    self.coll.find().each do |doc|
+      doc['drgs'] = []
+      results << doc
+    end
+    results
+  end
+
+  def insert(doc)
+    return :no_permission unless self.write
+    self.coll.insert(doc)
   end
 
 end
--- CHOP_drg_inserter/code/runscript.rb
+++ CHOP_drg_inserter/code/runscript.rb
@@ -8,7 +8,23 @@ db = 'chop_2013_ch'
 
 puts "connecting..."
 parser = Parser.new('../chop_related_drg.csv')
-de_read_adapter = Adapter.new(db,'de',host, port)
-#{de_read_adapter = Adapter.new(db,'test_coll',host, port)}
-puts de_read_adapter.get_entries
-puts parser.get_lines
+de_read_adapter = Adapter.new(db,'de',host, port, false)
+de_write_adapter = Adapter.new(db,'test_coll',host, port, true)
+chop_entries = de_read_adapter.get_entries
+chop_drg_assocs = parser.read_assocs
+results = []
+chop_drg_assocs.each do |assoc|
+  puts assoc['chop']
+
+    found_entries = chop_entries.select{|entry|
+      entry['code'].eql? assoc['chop']
+    }
+    unless found_entries[0].nil?
+      found_entries[0]['drgs']<<assoc['drg']
+    end
+end
+puts 'inserting...'
+chop_entries.each do |entry|
+  de_write_adapter.insert(entry)
+end
+
