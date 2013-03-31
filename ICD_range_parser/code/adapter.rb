bf63e04f1577252730d6a1e735ba8b7d07b65347
Gian-Luca Mateo <gianluca.mateo@gmail.com>
2013-03-31 18:02:21 +0200

    Added parser for ICD ranges. DB scheme updated shortly
    	modified:   CHOP_drg_inserter/.idea/workspace.xml
    	new file:   ICD_range_parser/.idea/.name
    	new file:   ICD_range_parser/.idea/ICD_range_parser.iml
    	new file:   ICD_range_parser/.idea/encodings.xml
    	new file:   ICD_range_parser/.idea/misc.xml
    	new file:   ICD_range_parser/.idea/modules.xml
    	new file:   ICD_range_parser/.idea/scopes/scope_settings.xml
    	new file:   ICD_range_parser/.idea/vcs.xml
    	new file:   ICD_range_parser/.idea/workspace.xml
    	new file:   ICD_range_parser/code/adapter.rb
    	new file:   ICD_range_parser/code/parser.rb
    	new file:   ICD_range_parser/code/runscript.rb
    	new file:   ICD_range_parser/ranges.csv

---
+++ ICD_range_parser/code/adapter.rb
@@ -0,0 +1,29 @@
+require 'mongo'
+include Mongo
+
+class Adapter
+  attr_accessor :mongo_client, :db, :coll, :write
+  def initialize (db , collection ,host, port, write)
+    mongo_client = MongoClient.new(host, port)
+    self.write = write
+    if write
+      puts 'Enter your PW for the account pse4_write: '
+      mongo_client.db('admin').authenticate('pse4_write',gets.chomp)
+    else
+      mongo_client.db('admin').authenticate('pse4_read','plokij')
+    end
+    db = mongo_client.db(db)
+    self.coll = db.collection(collection)
+  end
+
+  def drop_collection
+    self.coll.remove()
+  end
+
+  def insert(doc)
+    return :no_permission unless self.write
+    self.coll.insert(doc)
+  end
+
+end
+
